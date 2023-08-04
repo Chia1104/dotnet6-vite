613f0c2d87a8a548a266f3bb426dd2e0735bbbab
Chia1104 <yuyuchia7423@gmail.com>
2023-08-04 17:50:45 +0800

    chore: seed data

--- dotnet6-vite/Helpers/DataContext.cs
+++ dotnet6-vite/Helpers/DataContext.cs
@@ -33,6 +33,53 @@ public class DataContext : DbContext
             .HasOne(u => u.Weapon)
             .WithMany(w => w.Users)
             .HasForeignKey(u => u.WeaponId);
+        
+        modelBuilder.Entity<Armor>().HasData(new Armor
+        {
+            ArmorId = Guid.NewGuid(),
+            Name = "First Armor",
+            Description = "A simple armor",
+            Defense = 1,
+            Level = 1,
+            Heaviness = 1,
+            Image = "https://firebasestorage.googleapis.com/v0/b/dotnet6-vite-mmorpg.appspot.com/o/images%2Farmors%2Farmor%20(2).png?alt=media",
+        });
+        
+        modelBuilder.Entity<Headgear>().HasData(new Headgear
+        {
+            HeadgearId = Guid.NewGuid(),
+            Name = "First Headgear",
+            Description = "A simple headgear",
+            Defense = 1,
+            Level = 1,
+            Heaviness = 1,
+            Image = "https://firebasestorage.googleapis.com/v0/b/dotnet6-vite-mmorpg.appspot.com/o/images%2Fheadgears%2Fhelmet%20(1).png?alt=media",
+        });
+        
+        modelBuilder.Entity<Shield>().HasData(new Shield
+        {
+            ShieldId = Guid.NewGuid(),
+            Name = "First Shield",
+            Description = "A simple shield",
+            Defense = 1,
+            Level = 1,
+            Heaviness = 1,
+            Attack = 1,
+            Image = "https://firebasestorage.googleapis.com/v0/b/dotnet6-vite-mmorpg.appspot.com/o/images%2Fweapons%2Faward.png?alt=media",
+        });
+        
+        modelBuilder.Entity<Weapon>().HasData(new Weapon
+        {
+            WeaponId = Guid.NewGuid(),
+            Name = "First Weapon",
+            Description = "A simple weapon",
+            Defense = 1,
+            Level = 1,
+            Heaviness = 1,
+            Attack = 1,
+            Image = "https://firebasestorage.googleapis.com/v0/b/dotnet6-vite-mmorpg.appspot.com/o/images%2Fweapons%2Fdagger%20(1).png?alt=media",
+        });
+        
     }
 
     public DbSet<Example> Examples { get; set; }
